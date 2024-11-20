c1c0a7064ca62ea2b90d61d3c858247993c002aa
polite <145156622+BEALpolite@users.noreply.github.com>
2024-11-20 17:49:54 +0100

    fix two errors in ragdoll script

--- gamemode_hoodlums/gamemodes/homemade_hood/gamemode/player/ragdoll/sv_ragdoll.lua
+++ gamemode_hoodlums/gamemodes/homemade_hood/gamemode/player/ragdoll/sv_ragdoll.lua
@@ -372,7 +372,7 @@ hook.Add("PostEntityTakeDamage", "aassadassa", function(ent, dmginfo, what)
                     -- do limb damage
                     if RagdollHitGroups[bonename] then
                         local hitgroup = RagdollHitGroups[bonename]
-                        if IsValid(owner) and owner:Alive() then
+                        if IsValid(owner) and owner:IsPlayer() and owner:Alive() then
                             owner:TakeLimbDamage(hitgroup, dmg, false)
                         end
                      end
@@ -387,7 +387,7 @@ hook.Add("PostEntityTakeDamage", "aassadassa", function(ent, dmginfo, what)
                             ent.headshot = true
 
                             local owner = ent:GetOwner()
-                            if IsValid(owner) and owner:Alive() then
+                            if IsValid(owner) and owner:IsPlayer() and owner:Alive() then
                                 owner:SetLastHitGroup(HITGROUP_HEAD)
                                 KillPlayerDamageInfo(owner, dmginfo)
                             end
